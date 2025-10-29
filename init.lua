eda73428950fc536da3a8c825dbd2408e1f0fa97
robkisk <robby.kiskanyan@databricks.com>
2025-10-29 14:26:03 -0400

    adding initial code

---
+++ init.lua
@@ -0,0 +1,6 @@
+require("config.lazy")
+require("config.options")
+require("config.autocmds")
+require("config.utils")
+require("config.mappings")
+
---
+++ lua/toggleterm.lua
@@ -0,0 +1,15 @@
+-- local set = vim.keymap.set
+-- local Term = require("toggleterm.terminal").Terminal
+-- local lazygit = Term:new({
+--   cmd = "lazygit",
+--   direction = "float",
+--   hidden = true
+-- })
+--
+-- function _lazygit_toggle()
+--   lazygit:toggle()
+-- end
+--
+-- set("n", "<leader>lg", "<cmd>lua _lazygit_toggle()<CR>")
+-- set("n", "<leader>tt", "<cmd>ToggleTerm direction=tab<CR>")
+-- set("n", "<leader>tf", "<cmd>ToggleTerm direction=float<CR>")
